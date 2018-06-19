5b1cca0022b66511c5934ad0fbd936614ed0ec6c
Thinker Pal <thinkerpal@gmail.com>
2018-06-19 23:14:42 +0800

    Adding Settings VC Swift files

---
+++ Todolist/Todolist/Settings Options/HelpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HelpViewController.swift
+//  Todolist
+//
+//  Created by Rui Yang Tan on 19/6/18.
+//  Copyright © 2018 Rui Yang Tan. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
