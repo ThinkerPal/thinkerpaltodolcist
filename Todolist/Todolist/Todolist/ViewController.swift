127179c76985af188f82d09d0729b0ec588c1b72
Thinker Pal <thinkerpal@gmail.com>
2018-06-19 16:39:17 +0800

    Initial Commit

---
+++ Todolist/Todolist/Todolist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todolist
+//
+//  Created by Rui Yang Tan on 19/6/18.
+//  Copyright © 2018 Rui Yang Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
